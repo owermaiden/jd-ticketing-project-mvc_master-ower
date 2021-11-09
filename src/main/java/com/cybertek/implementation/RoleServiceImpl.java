237ae5f1d9c8b9d7ab2eb6f231f4dbfa5af1bdd4
owermaiden <ower.maiden@cybertekschool.com>
2021-11-09 22:24:40 +0300

    Role Service Implementation

---
+++ src/main/java/com/cybertek/implementation/RoleServiceImpl.java
@@ -0,0 +1,34 @@
+package com.cybertek.implementation;
+
+import com.cybertek.dto.RoleDTO;
+import com.cybertek.service.RoleService;
+
+import java.util.List;
+
+public class RoleServiceImpl extends AbstractMapService<RoleDTO, Long> implements RoleService {
+
+    @Override
+    public List<RoleDTO> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public RoleDTO findById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public RoleDTO save(RoleDTO object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(RoleDTO object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteById(aLong);
+    }
+}
