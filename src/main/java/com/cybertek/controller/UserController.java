d92105f30ed137ffe549804975f485e7360d3ef6
owermaiden <ower.maiden@cybertekschool.com>
2021-11-09 17:26:07 +0300

    User Create

---
+++ src/main/java/com/cybertek/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.cybertek.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/create")
+    public String createUser(){
+
+        return "/user/create";
+    }
+}
