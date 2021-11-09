98b4e4d48a2ab0f7351a50fc58b92a31caa1c01b
owermaiden <ower.maiden@cybertekschool.com>
2021-11-09 22:15:35 +0300

    User Service Implementation

---
+++ src/main/java/com/cybertek/implementation/UserServiceImpl.java
@@ -0,0 +1,33 @@
+package com.cybertek.implementation;
+
+import com.cybertek.dto.UserDTO;
+import com.cybertek.service.UserService;
+
+import java.util.List;
+
+public class UserServiceImpl extends AbstractMapService<UserDTO, String> implements UserService {
+    @Override
+    public List<UserDTO> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(String id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(UserDTO object) {
+        super.delete(object);
+    }
+
+    @Override
+    public UserDTO save(UserDTO object) {
+        return super.save(object.getUserName(), object);
+    }
+
+    @Override
+    public UserDTO findById(String id) {
+        return super.findById(id);
+    }
+}
