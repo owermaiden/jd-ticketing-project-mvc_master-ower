b181299abd5354059fbfbe7246881fb2b4baa0a1
owermaiden <ower.maiden@cybertekschool.com>
2021-11-09 17:01:56 +0300

    Initial commit

---
+++ src/main/java/com/cybertek/controller/LoginController.java
@@ -0,0 +1,19 @@
+package com.cybertek.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+	
+	@RequestMapping
+	public String login(){
+		return "login";
+	}
+	
+	@RequestMapping("/welcome")
+	public String welcome(){
+		return "welcome";
+	}
+
+}
