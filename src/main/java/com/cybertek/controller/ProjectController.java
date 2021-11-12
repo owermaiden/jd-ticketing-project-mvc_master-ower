f29892ab867f8b95876c75cdfe3305d524a2e56e
owermaiden <ower.maiden@cybertekschool.com>
2021-11-12 15:25:27 +0300

    Project Finish

--- src/main/java/com/cybertek/controller/ProjectController.java
+++ src/main/java/com/cybertek/controller/ProjectController.java
@@ -1,12 +1,16 @@
 package com.cybertek.controller;
 
 import com.cybertek.dto.ProjectDTO;
+import com.cybertek.dto.UserDTO;
 import com.cybertek.service.ProjectService;
 import com.cybertek.service.UserService;
+import com.cybertek.utils.Status;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.stream.Collectors;
@@ -32,5 +36,41 @@ public class ProjectController {
         //model.addAttribute("managers", userService.findAll().stream().filter(user -> user.getRole().getDescription().equals("manager")).collect(Collectors.toList()));
 
         return "/project/create";
+    }
+
+    @PostMapping("/create")
+    // public String saveUser(@ModelAttribute("user") UserDTO user, Model model){   // we dont need to use @ModelAttribute anymore......
+    public String saveProject(ProjectDTO project, Model model){
+
+        projectService.save(project);
+        project.setProjectStatus(Status.OPEN);
+
+        return "redirect:/project/create";  // redirect calls the GetMapping instead of view...
+    }
+
+    @GetMapping("/update/{projectCode}")
+    public String editProject(@PathVariable("projectCode") String projectCode, Model model){
+
+        model.addAttribute("project", projectService.findById(projectCode));
+        model.addAttribute("projects", projectService.findAll());
+        model.addAttribute("managers", userService.findAll());
+
+        return "/project/update";
+
+    }
+
+    @PostMapping("/update/{projectCode}")
+    public String updateProject(@PathVariable("projectCode") String projectcode,ProjectDTO project, Model model){
+
+        projectService.update(project);
+        return "redirect:/project/create";  // redirect calls the GetMapping instead of view...
+    }
+
+    @GetMapping("/delete/{projectCode}")
+    public String deleteProjectById(@PathVariable("projectCode") String projectcode, Model model){
+
+        projectService.deleteById(projectcode);
+        return "redirect:/project/create";  // redirect calls the GetMapping instead of view...
+
     }
 }
---
+++ src/main/java/com/cybertek/converter/UserDtoConverter.java
@@ -0,0 +1,23 @@
+package com.cybertek.converter;
+
+import com.cybertek.dto.RoleDTO;
+import com.cybertek.dto.UserDTO;
+import com.cybertek.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationPropertiesBinding
+public class UserDtoConverter implements Converter<String, UserDTO> {
+
+    @Autowired
+    UserService userService;
+
+    @Override
+    public UserDTO convert(String source) {
+
+        return userService.findById(source);
+    }
+}
