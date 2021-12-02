14024128c6eec1e53b136d888b2b379906436f4e
owermaiden <ower.maiden@cybertekschool.com>
2021-12-02 16:33:19 +0300

    Project Table

--- src/main/java/com/cybertek/dto/UserDTO.java
+++ src/main/java/com/cybertek/dto/UserDTO.java
@@ -12,6 +12,8 @@ import lombok.Setter;
 @Setter
 public class UserDTO {
 
+    private Long id;
+
     private String firstName;
     private String lastName;
     private String userName;
--- src/main/java/com/cybertek/implementation/ProjectServiceImpl.java
+++ src/main/java/com/cybertek/implementation/ProjectServiceImpl.java
@@ -2,7 +2,9 @@ package com.cybertek.implementation;
 
 import com.cybertek.dto.ProjectDTO;
 import com.cybertek.entity.Project;
+import com.cybertek.enums.Status;
 import com.cybertek.mapper.ProjectMapper;
+import com.cybertek.mapper.UserMapper;
 import com.cybertek.repository.ProjectRepository;
 import com.cybertek.service.ProjectService;
 import org.springframework.stereotype.Service;
@@ -13,12 +15,14 @@ import java.util.stream.Collectors;
 @Service
 public class ProjectServiceImpl implements ProjectService {
 
-    ProjectRepository projectRepository;
-    ProjectMapper projectMapper;
+    private ProjectRepository projectRepository;
+    private ProjectMapper projectMapper;
+    private UserMapper userMapper;
 
-    public ProjectServiceImpl(ProjectRepository projectRepository, ProjectMapper projectMapper) {
+    public ProjectServiceImpl(ProjectRepository projectRepository, ProjectMapper projectMapper, UserMapper userMapper) {
         this.projectRepository = projectRepository;
         this.projectMapper = projectMapper;
+        this.userMapper = userMapper;
     }
 
     @Override
@@ -35,7 +39,9 @@ public class ProjectServiceImpl implements ProjectService {
 
     @Override
     public void save(ProjectDTO dto) {
+        dto.setProjectStatus(Status.OPEN);
         Project project = projectMapper.convertToEntity(dto);
+        project.setAssignedManager(userMapper.convertToEntity(dto.getAssignedManager()));
         projectRepository.save(project);
     }
 
